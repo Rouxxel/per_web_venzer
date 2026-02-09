110aad5299544b207b9af1c7a59a2ab9491bf5ba
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-02-09 21:36:20 +0100

    Implement tailwind styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className="text-red-500">
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
