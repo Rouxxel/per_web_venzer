545baa443e5f61996f1a8cdbd0a8060f835bf8b5
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-03-30 10:53:45 +0200

    Minor changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,4 +2,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Always start at the top on page load/reload
+window.history.scrollRestoration = "manual";
+window.scrollTo(0, 0);
+
 createRoot(document.getElementById("root")!).render(<App />);
