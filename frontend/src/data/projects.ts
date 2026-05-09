690631d2c23a5a7ab5ba6f4f88bccd0241e39f58
Rouxxel <106406623+Rouxxel@users.noreply.github.com>
2026-05-09 14:01:56 +0200

    Update showcase video

--- frontend/src/data/projects.ts
+++ frontend/src/data/projects.ts
@@ -138,7 +138,7 @@ export const projects_en: Project[] = [
     ],
     classifications: ["Fullstack", "Web", "Database", "AI / Data", "Hackathon"],
     github: "https://github.com/peres84/TuneTools",
-    demo: "https://vimeo.com/1143501418?fl=pl&fe=vl",
+    demo: "https://youtu.be/MqDOA6DquCQ",
   },
   {
     title: "Bachelor Thesis (Machine Learning)",
